32fad9ad8acb623c6565a1be5071903c72ffe2d7
mahmoud-eltahawy <gemmy9300@gmail.com>
2024-03-16 00:31:07 +0200

    welcome page done
    the background blur effect is not implemented yet

--- src/main.jsx
+++ src/main.jsx
@@ -34,7 +34,7 @@ const router = createBrowserRouter([
     element : <AccountProfilePage/>
   },
   {
-    path : "/welcomePage",
+    path : "/welcome",
     element : <WelcomePage/>
   },
   {
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,8 +1,16 @@
-
+import { useSearchParams } from "react-router-dom";
+// @ts-ignore
+import logo from "../assets/logo/logo-dark.svg"
 
 
 export default function WelcomePage() {
+  const [params,_] = useSearchParams();
   return (
-    <h1 className="text-3xl">Welcome page</h1>
+    <section>
+      <img src={logo} alt="site logo" className="w-20 absolute right-2 top-2"/>
+      <span className="h-screen flex items-center justify-center text-7xl text-white drop-shadow-2xl">
+        {"اهلا وسهلا بك يا " + params.get("name")}
+      </span>
+    </section>
   )
 }
