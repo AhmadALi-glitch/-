056a7096e9a83bff03d1bdc46f6c25e211dca518
POWR_Abdelrahman <powrabdelrahman@gmail.com>
2024-03-17 23:15:14 +0200

    Commit changes

--- src/pages/Guide.jsx
+++ src/pages/Guide.jsx
@@ -1,10 +1,137 @@
-
+import '../../app/guide.css';
+import  { Book } from "phosphor-react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default function GuidePage() {
-    return (
-        <>
-            Guide Page
-        </>
-    )
+  return (
+    <section className="guide-page">
+      <div className="page-wrapper">
+        <div className="content">
+          <h2 className="guide-heading">الدليل</h2>
+          <div className="faqs-wrapper">
+            <AccordionDemo />
+          </div>
+        </div>
+
+        <aside className="guide-sidebar">
+          <h3 className="header">دليلك لإستخدام المنصة</h3>
+          <div className="body">
+            <input placeholder="ابحث عن" name="search-query" id="guideSearchInput" />
+            <span className="icon">
+              <Book />
+            </span>
+          </div>
+        </aside>
+      </div>
+    </section>
+  );
 }
 
+function AccordionDemo() {
+  return (
+    <Accordion type="single" collapsible className="w-full">
+      <AccordionItem value="item-1">
+        <AccordionTrigger>كيف انضم إلي فريق؟</AccordionTrigger>
+        <AccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-2">
+        <AccordionTrigger>كيف انضم إلي فريق؟</AccordionTrigger>
+        <AccordionContent>
+          Yes. It comes with default styles that matches the other
+          components&apos; aesthetic.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger>كيف انضم إلي فريق؟</AccordionTrigger>
+        <AccordionContent>
+          Yes. It&apos;s animated by default, but you can disable it if you
+          prefer.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-4">
+        <AccordionTrigger>كيف انضم إلي فريق؟</AccordionTrigger>
+        <AccordionContent>
+          Yes. It&apos;s animated by default, but you can disable it if you
+          prefer.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-5">
+        <AccordionTrigger>كيف انضم إلي فريق؟</AccordionTrigger>
+        <AccordionContent>
+          Yes. It&apos;s animated by default, but you can disable it if you
+          prefer.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-6">
+        <AccordionTrigger>كيف انضم إلي فريق؟</AccordionTrigger>
+        <AccordionContent>
+          Yes. It&apos;s animated by default, but you can disable it if you
+          prefer.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-7">
+        <AccordionTrigger>كيف انضم إلي فريق؟</AccordionTrigger>
+        <AccordionContent>
+          Yes. It&apos;s animated by default, but you can disable it if you
+          prefer.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-8">
+        <AccordionTrigger>كيف انضم إلي فريق؟</AccordionTrigger>
+        <AccordionContent>
+          Yes. It&apos;s animated by default, but you can disable it if you
+          prefer.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-9">
+        <AccordionTrigger>كيف انضم إلي فريق؟</AccordionTrigger>
+        <AccordionContent>
+          Yes. It&apos;s animated by default, but you can disable it if you
+          prefer.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-10">
+        <AccordionTrigger>كيف انضم إلي فريق؟</AccordionTrigger>
+        <AccordionContent>
+          Yes. It&apos;s animated by default, but you can disable it if you
+          prefer.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-11">
+        <AccordionTrigger>كيف انضم إلي فريق؟</AccordionTrigger>
+        <AccordionContent>
+          Yes. It&apos;s animated by default, but you can disable it if you
+          prefer.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-12">
+        <AccordionTrigger>كيف انضم إلي فريق؟</AccordionTrigger>
+        <AccordionContent>
+          Yes. It&apos;s animated by default, but you can disable it if you
+          prefer.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-13">
+        <AccordionTrigger>كيف انضم إلي فريق؟</AccordionTrigger>
+        <AccordionContent>
+          Yes. It&apos;s animated by default, but you can disable it if you
+          prefer.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-14">
+        <AccordionTrigger>كيف انضم إلي فريق؟</AccordionTrigger>
+        <AccordionContent>
+          Yes. It&apos;s animated by default, but you can disable it if you
+          prefer.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+}
