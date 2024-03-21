7c00a0dc50bf40e95f1ada374bcb0abbe01b00a2
Ahmad Ali <newTownElectroo@gmail.com>
2024-03-21 23:46:46 +0300

    change the design

--- src/main.jsx
+++ src/main.jsx
@@ -49,28 +49,38 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
     <div className="bg-background w-svw h-svh pl-10 pr-10 pt-6">
 
-      <div className="w-full flex justify-between items-end border-b-[1px] pb-1 border-[#C9DF8A] text-[#C9DF8A]">
+      <div className="w-full border-b-[3px] border-[#A8BE69] pb-[1px] border-opacity-[0.1] flex justify-between items-center text-[#C9DF8A]">
+
         <div className="logo">
           <img width={50} src="src/assets/logo/logo-dark.svg"></img>
         </div>
-        <div className="pages flex items-center justify-center gap-5 basis-[50%]">
-          <div className="cursor-pointer flex items-center gap-5 basis-[20%] rounded-xl events-nav">
-            <Flag size={30} className="text-[#C9DF8A]"/>
-            <div>الفعاليات</div>
+
+        <div className="pages flex items-center justify-end gap-2 basis-[50%]">
+
+          <div className="group-1 flex border-2 gap-5 border-[#C9DF8A] text-[#333] text-sm font-extrabold pl-2 pr-2 p-1 rounded-[5px] bg-[#C9DF8A]">
+            <div className="cursor-pointer bg-[#A8BE69] flex items-center gap-5 basis-[20%] pr-2 pl-2 p-[2px] rounded-[4px] events-nav">
+              <Flag size={25}/>
+              <div>الفعاليات</div>
+            </div>
+            <div className="flex cursor-pointer items-center gap-5 teams-nav">
+              <UsersThree size={25} />
+              {/* <div>الفرق</div> */}
+            </div>
+            <div className="flex cursor-pointer items-center gap-5 home-nav">
+              <House size={25}/>
+              {/* <div>المنزل</div> */}
+            </div>
           </div>
-          <div className="flex cursor-pointer items-center gap-5 teams-nav">
-            <UsersThree size={30} className="text-[#C9DF8A]"/>
-            <div>الفرق</div>
+          
+          <div className="group-2 border-2 rounded-[5px] text-sm border-[#C9DF8A] p-[6px]">
+            <div className="flex cursor-pointer items-center gap-2 pl-1 pr-1 profile">
+              <UserCircle size={25} className="text-[#C9DF8A]"/>
+              {/* <div>صفحتي الشخصية</div> */}
+            </div>
           </div>
-          <div className="flex cursor-pointer items-center gap-5 home-nav">
-            <House size={30} className="text-[#C9DF8A]"/>
-            <div>المنزل</div>
-          </div>
+
         </div>
-        <div className="flex cursor-pointer items-center gap-5 profile">
-          <UserCircle size={30} className="text-[#C9DF8A]"/>
-            <div>صفحتي الشخصية</div>
-        </div>
+
       </div>
 
       <RouterProvider router={router}/>
