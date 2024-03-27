bee583cd1b61366339e09d5995e3010f139099ad
POWR_Abdelrahman <powrabdelrahman@gmail.com>
2024-03-28 01:57:47 +0200

    Home

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,56 @@
 
-import { Flag, UserCircle, UsersThree, House } from "phosphor-react"
+// import { Flag, UserCircle, UsersThree, House } from "phosphor-react"
+import { ArrowCircleLeft } from "phosphor-react";
+import '../../app/home.css';
+
 
 export default function MainPage() {
   return (
-    <>main</>
+    <>
+      <div className="responsive-table">
+        <table>
+          <thead>
+            <tr>
+              <td></td>
+              <td colSpan="8">الفعاليات</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="main-cell">الفعاليات الجارية</td>
+              <td><EventBox /></td>
+              <td><EventBox /></td>
+              <td><EventBox /></td>
+              <td><EventBox /></td>
+              <td><EventBox /></td>
+              <td><EventBox /></td>
+              <td><EventBox /></td>
+              <td><EventBox /></td>
+            </tr>
+            <tr>
+              <td className="main-cell">الفعاليات القادمة</td>
+              <td><EventBox /></td>
+              <td><EventBox /></td>
+              <td><EventBox /></td>
+              <td><EventBox /></td>
+              <td><EventBox /></td>
+              <td><EventBox /></td>
+              <td><EventBox /></td>
+              <td><EventBox /></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
   )
+}
+
+function EventBox() {
+  return (
+    <div className="box">
+      <h3>ماراثون كتابة</h3>
+      <p>شرح شرح شرح شرح</p>
+      <a href="#"><ArrowCircleLeft /></a>
+    </div>
+  );
 }
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,33 +1,37 @@
-import { UsersThree, Flag, House, UserCircle } from "phosphor-react"
+import { UsersThree, Flag, House, UserCircle } from "phosphor-react";
+import { useState } from "react";
+import '../../app/header.css';
 
 export default function Navbar() {
-    return (
-        <>
-            <div className="pages flex items-center justify-end gap-2 basis-[20%]">
+  const [isActive, setIsActive] = useState("events");
 
-                <div className="group-1 flex border-2 gap-5 border-[#C9DF8A] text-[#333] text-sm font-extrabold pl-2 pr-2 p-1 rounded-[5px] bg-[#C9DF8A]">
-                <div className="cursor-pointer bg-[#A8BE69] flex items-center gap-5 basis-[20%] pr-2 pl-2 p-[2px] rounded-[4px] events-nav">
-                    <Flag size={25}/>
-                    <div>الفعاليات</div>
-                </div>
-                <div className="flex cursor-pointer items-center gap-5 teams-nav">
-                    <UsersThree size={25} />
-                    {/* <div>الفرق</div> */}
-                </div>
-                <div className="flex cursor-pointer items-center gap-5 home-nav">
-                    <House size={25}/>
-                    {/* <div>المنزل</div> */}
-                </div>
-                </div>
-            
-                <div className="group-2 border-2 rounded-[5px] text-sm border-[#C9DF8A] p-[6px]">
-                <div className="flex cursor-pointer items-center gap-2 pl-1 pr-1 profile">
-                    <UserCircle size={25} className="text-[#C9DF8A]"/>
-                    {/* <div>صفحتي الشخصية</div> */}
-                </div>
-                </div>
+  return (
+    <>
+      <div className="pages flex items-center justify-end gap-2 basis-[20%]">
 
-            </div>
-        </>
-    )
+        <div className="group-1 flex border-2 gap-5 border-[#C9DF8A] text-[#333] text-sm font-extrabold pl-2 pr-2 p-1 rounded-[5px] bg-[#C9DF8A]">
+          <div className={isActive === "events" ? "active cursor-pointer bg-[#A8BE69] flex items-center gap-5 basis-[20%] pr-2 pl-2 p-[2px] rounded-[4px] events-nav" : "flex cursor-pointer items-center gap-5 teams-nav"} onClick={() => setIsActive("events")}>
+            <Flag size={25} />
+            <span>الفعاليات</span>
+          </div>
+          <div className={isActive === "teams" ? "active cursor-pointer bg-[#A8BE69] flex items-center gap-5 basis-[20%] pr-2 pl-2 p-[2px] rounded-[4px] events-nav" : "flex cursor-pointer items-center gap-5 teams-nav"} onClick={() => setIsActive("teams")}>
+            <UsersThree size={25} />
+            <span>الفرق</span>
+          </div>
+          <div className={isActive === "home" ? "active cursor-pointer bg-[#A8BE69] flex items-center gap-5 basis-[20%] pr-2 pl-2 p-[2px] rounded-[4px] events-nav" : "flex cursor-pointer items-center gap-5 teams-nav"} onClick={() => setIsActive("home")}>
+            <House size={25} />
+            <span>المنزل</span>
+          </div>
+        </div>
+
+        <div className="group-2 border-2 rounded-[5px] text-sm border-[#C9DF8A] p-[6px]">
+          <div className="flex cursor-pointer items-center gap-2 pl-1 pr-1 profile">
+            <UserCircle size={25} className="text-[#C9DF8A]" />
+            {/* <div>صفحتي الشخصية</div> */}
+          </div>
+        </div>
+
+      </div>
+    </>
+  );
 }
